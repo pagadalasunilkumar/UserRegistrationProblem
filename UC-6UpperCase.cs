3a5a2cfc4f34c74237cf6f51d5e60501f8803ba0
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-06-09 13:23:53 +0530

    [sunil] the file is added

---
+++ UC-6UpperCase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UC_6UpperCase
+    {
+        static void Main()
+        {
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            bool isValidPassword = IsPasswordValid(password);
+
+            if (isValidPassword)
+            {
+                Console.WriteLine("Valid password.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid password. Password must have a minimum of 8 characters and at least 1 uppercase letter.");
+            }
+        }
+
+        static bool IsPasswordValid(string password)
+        {
+            if (password.Length >= 8 && Regex.IsMatch(password, @"[A-Z]"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
