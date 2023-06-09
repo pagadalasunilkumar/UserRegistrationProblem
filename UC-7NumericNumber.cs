f201ea0a74926a2acd23ca38e05afdaaf0b13dba
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-06-09 13:26:57 +0530

    [sunil] the file is added

---
+++ UC-7NumericNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UC_7NumericNumber
+    {
+        static void Main()
+        {
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            bool isValidPassword = IsPasswordValid(password);
+
+            if (isValidPassword)
+            {
+                Console.WriteLine("Valid password.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid password. Password must have a minimum of 8 characters, at least 1 uppercase letter, and at least 1 numeric digit.");
+            }
+        }
+
+        static bool IsPasswordValid(string password)
+        {
+            if (password.Length >= 8 && Regex.IsMatch(password, @"[A-Z]") && Regex.IsMatch(password, @"\d"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
