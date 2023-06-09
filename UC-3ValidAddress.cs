06500d2f382f70697f9851243070ff29dae5a33a
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-06-09 13:13:36 +0530

    [sunil] the file is added

---
+++ UC-3ValidAddress.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UC_3ValidAddress
+    {
+        static void Main()
+        {
+            Console.Write("Enter your email address: ");
+            string email = Console.ReadLine();
+
+            bool isValidEmail = Regex.IsMatch(email, @"^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$");
+
+            if (isValidEmail)
+            {
+                Console.WriteLine("Valid email address.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address.");
+            }
+        }
+    }
+}
