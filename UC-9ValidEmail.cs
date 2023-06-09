0fca261a962639c866de55d8d0efdab2f40c71a3
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-06-09 13:33:50 +0530

    [sunil] the file is added

---
+++ UC-9ValidEmail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UC_9ValidEmail
+    {
+        static void Main()
+        {
+            string[] emailSamples = {
+            "abc.xyz@bl.co.in",
+            "john.doe@example.com",
+            "test.email@example.co.uk",
+            "jane.doe@example.com",
+            "invalid.email",
+            "another.invalid.email@"
+        };
+
+            foreach (string email in emailSamples)
+            {
+                bool isValidEmail = IsEmailValid(email);
+
+                if (isValidEmail)
+                {
+                    Console.WriteLine($"Valid email: {email}");
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid email: {email}");
+                }
+            }
+        }
+
+        static bool IsEmailValid(string email)
+        {
+            return Regex.IsMatch(email, @"^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$");
+        }
+    }
+}
